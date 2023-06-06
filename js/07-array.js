6136697eed52409515c988ea2526fde27126f059
Vaidas D <vaidasdbr@gmail.com>
2023-06-05 23:53:03 -0700

    projektas7

--- js/07-array.js
+++ js/07-array.js
@@ -4,9 +4,9 @@ console.log(abc[0],abc[1]);
 console.log(abc.length);
 
 function num(n1) {
-return n1[0] + n1[1];
+return n1[0]+n1[1]+[n1[1]];
 }
-let abc2 = [1,2];
+let abc2 = [1,5];
 let abc3 = num(abc2);
 console.log(abc3);
 
@@ -14,4 +14,11 @@ let abc4 = ["Vaidas","Birute","Paskutinis"];
 let abc5 = abc4.length -1;
 console.log(abc4[abc5]);
 
-
+function arr(s1,s2) {
+return "\n"+s1[0]+" "+s1[1]+" "+(2023-s1[2]);
+//return `\n${w1[0]}.${w2} age ${2023-n1}`;
+}
+let arr1 = ["Pamela","Anderson",1970];
+let arr2 = ["Angelina","Jolie",1980];
+let arr3 = arr(arr1,arr2);
+console.log(arr3);
---
+++ js/08-if.js
@@ -0,0 +1,15 @@
+console.clear();
+
+
+if (1 === 1) {console.log("Lygu");}
+if ("1" !== 1) {console.log("Nelygu");}
+if ("1" >= 1) {console.log("Daugiau arba lygu");}
+
+function arr(s1,s2) {
+    return [s1[2]]<[s2[2]];
+    //return `\n${w1[0]}.${w2} age ${2023-n1}`;
+    }
+    let arr1 = ["Pamela","Anderson",1970];
+    let arr2 = ["Angelina","Jolie",1980];
+    let arr3 = arr(arr1,arr2);
+    console.log(arr3);

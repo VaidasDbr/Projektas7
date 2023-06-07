00e674a038341feb5a1043096e69d83e2e028192
Vaidas D <vaidasdbr@gmail.com>
2023-06-06 21:45:09 -0700

    projektas7

--- js/08-if.js
+++ js/08-if.js
@@ -1,10 +1,5 @@
 console.clear();
 
-
-if (1 === 1) {console.log("Lygu");}
-if ("1" !== 1) {console.log("Nelygu");}
-if ("1" >= 1) {console.log("Daugiau arba lygu");}
-
 function arr(s1,s2) {
     return [s1[2]]<[s2[2]];
     //return `\n${w1[0]}.${w2} age ${2023-n1}`;
---
+++ js/09-for.js
@@ -0,0 +1,25 @@
+let pazymiai = [6,4,5,5,7]
+for (let i=0; i<pazymiai.length; i++) {
+console.log(i);
+}
+
+
+
+
+
+let masyvas1 = [1,1,1];
+let masyvas2 = [1,2,2,3];
+let masyvas3 = [5,4,3,2,1];
+let masyvas4 = [5,4,3,2,1,1,1,1,1,2,2,3];
+let num11=0;let num22=0;let num33=0;let num44=0;let num55=0;
+for (let i7=0; i7<masyvas1.length; i7++) {
+if (masyvas1[i7] === 1) {num11++;}
+if (masyvas1[i7] === 2) {num22++;}
+if (masyvas1[i7] === 3) {num33++;}
+if (masyvas1[i7] === 4) {num44++;}
+if (masyvas1[i7] === 5) {num55++;}}
+console.log("masyvas1 turi 1 = "+num11);
+console.log("masyvas1 turi 2 = "+num22);
+console.log("masyvas1 turi 3 = "+num33);
+console.log("masyvas1 turi 4 = "+num44);
+console.log("masyvas1 turi 5 = "+num55);
